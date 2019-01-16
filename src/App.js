18add3ace377e24f978ec1df118a9b010860dcec
Sverre Johann Bjørke <sverrejb@gmail.com>
2019-01-16 23:43:36 +0100

    added moment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import moment from 'moment-timezone';
+import { cityData } from './cities'
 import './App.css';
 
+
 class App extends Component {
+
+  componentDidMount(){
+    console.log(moment().tz("America/Los_Angeles").format());
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>foo</h1>
       </div>
     );
   }
